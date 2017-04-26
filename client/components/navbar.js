b3e320875c3383676378453d09c1f2a7d2e502e0
Emily Sun <ESun0703@Emilys-MacBook-Pro.local>
2017-04-25 21:24:23 -0400

    added dropdown in navbar component

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -11,9 +11,14 @@ import React from "react";
 	        		<div className="container-fluid">
 	        			<img id="navImg" src="../public/style/mainLogo.jpg"/>
 	            		<ul className="nav navbar-nav navbar-right">
-	                		<li><a href="#">Sign Up</a></li>
-	                		<li><a href="#">Login</a></li>
-	            		</ul>
+					        <li className="dropdown">
+					          <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Sign In <span className="caret"></span></a>
+					          <ul className="dropdown-menu">
+					            <li><a href="#">Sign Up</a></li>
+					            <li><a href="#">Log In</a></li>
+					          </ul>
+					        </li>
+					    </ul>
 	        		</div>
 	    		</nav>
     		</div>
