f7fac483d7f1a71bbe3d73b81e24593f69f0cca3
Emily Sun <ESun0703@Emilys-MacBook-Pro.local>
2017-05-03 00:58:38 -0400

    fixed main.html formatting

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -14,19 +14,26 @@ import React from "react";
 		return(
 			<div className="row">
 				<nav className="navbar navbar-inverse">
-	        		<div className="container-fluid">
-	        			<img id="navImg" src="../public/style/mainLogo.jpg"/>
-	            		<ul className="nav navbar-nav navbar-right">
-					        <li className="dropdown">
-					          <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Sign In <span className="caret"></span></a>
-					          <ul className="dropdown-menu">
-					            <li><a href="#">Sign Up</a></li>
-					            <li><a href="#">Log In</a></li>
-					          </ul>
-					        </li>
-					    </ul>
-	        		</div>
-	    		</nav>
+		            <div className="container-fluid">
+		                <ul className="nav navbar-nav navbar-left">
+		                    <li><a href="#">Main</a></li>
+		                    <li><a href="#">Recipe</a></li>
+		                    <li><a href="#">Donation</a></li>                    
+		                </ul>
+		                <a href="./main.html">
+		                    <img className="mainlogo" src="./style/mainLogo.jpg"/>
+		                </a>
+		                <ul className="nav navbar-nav navbar-right">
+		                    <li className="dropdown">
+		                        <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Sign In <span className="caret"></span></a>
+		                            <ul className="dropdown-menu">
+		                                <li><a href="#">Sign Up</a></li>
+		                                <li><a href="#">Log In</a></li>
+		                            </ul>
+		                    </li>
+		                </ul>
+		            </div>
+        		</nav>
     		</div>
 		)	
     }
